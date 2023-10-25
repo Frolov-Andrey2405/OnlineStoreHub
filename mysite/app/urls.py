49bf764a866dad63e1b092aff60cbbfc14ff9cc0
Andrey Frolov <senior.slender2015@gmail.com>
2023-10-25 16:37:08 +0300

    URL name, namespace

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from app import views
 
+app_name = 'app'
+
 urlpatterns = [
     path('hello/', views.index),  # http://127.0.0.1:8000/hello/
-    path('detail/<int:my_id>/', views.indexItem),  # http://127.0.0.1:8000/hello/<int:my_id>
+    # http://127.0.0.1:8000/detail/<int:my_id>
+    path('detail/<int:my_id>/', views.indexItem, name='detail'),
     path('contact/', views.contact),  # http://127.0.0.1:8000/contact/
 ]
