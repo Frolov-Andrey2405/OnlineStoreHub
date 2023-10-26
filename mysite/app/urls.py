161ca965e2b648b3bc41fac802ac9178a08dbfef
Andrey Frolov <senior.slender2015@gmail.com>
2023-10-26 17:45:22 +0300

    Styles

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -5,6 +5,6 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.index, name='index'),  # http://127.0.0.1:8000/
-    path('detail/<int:my_id>/', views.indexItem, name='detail'), # http://127.0.0.1:8000/detail/<int:my_id>
-    path('add_item/', views.add_item, name='add_item'),
+    path('detail/<int:my_id>/', views.indexItem, name='detail'),  # http://127.0.0.1:8000/detail/<int:my_id>
+    path('add_item/', views.add_item, name='add_item'),  # http://127.0.0.1:8000/add_item/
 ]
