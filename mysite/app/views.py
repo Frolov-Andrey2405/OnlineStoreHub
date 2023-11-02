b6beefd8bee0a995c9f8a860e680f789cef6ddd9
Andrey Frolov <senior.slender2015@gmail.com>
2023-11-02 18:22:54 +0200

    create_checkout_session

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,4 +1,5 @@
 import json
+import stripe
 
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
@@ -10,7 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import DeleteView
 
-from .models import Product
+from .models import OrderDetail, Product
 
 
 def index(request):
@@ -86,3 +87,36 @@ def delete_item(request, my_id):
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy("app:index")
+
+
+@csrf_exempt
+def create_checkout_session(request, id):
+    product = get_object_or_404(Product, pk=id)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    checkout_session = stripe.checkout.Session.create(
+        customer_email=request.user.email,
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price-data": {
+                    "currency": "usd",
+                    "product_data": {
+                        "name": product.name,
+                    },
+                    "unit_amount": int(product.price),
+                },
+                "quantity": 1,
+            }
+        ],
+        mode="payment",
+        success_url=request.build_absolute_uri(reverse("app:success"))
+        + "?session_id+{CHECKOUT_SESSION_ID}",
+        cancel_url=request.build_absolute_uri(reverse("app:failed")),
+    )
+    order = OrderDetail()
+    order.customer_username = request.user.username
+    order.product = product
+    order.stripe_payment_intent = checkout_session["payment_intent"]
+    order.amount = int(product.price)
+    order.save()
+    return JsonResponse({"sessionId": checkout_session.id})
